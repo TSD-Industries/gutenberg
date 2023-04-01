aeafff623d354477de968120a8ab42adb542b885
jcavazos <joe@jlcavazos.com>
2023-04-01 14:54:28 -0500

    Init?

---
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -0,0 +1,10 @@
+package org.tsd.gutenberg;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class GutenbergRequestHandler implements RequestHandler<String, String> {
+    public String handleRequest(String s, Context context) {
+        return null;
+    }
+}
