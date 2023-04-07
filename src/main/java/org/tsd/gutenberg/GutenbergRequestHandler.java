c26824991567cb2090b0adbeec2521f18189d4cb
jcavazos <joe@jlcavazos.com>
2023-04-07 09:56:57 -0500

    API events.

--- src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -2,9 +2,17 @@ package org.tsd.gutenberg;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
-public class GutenbergRequestHandler implements RequestHandler<String, String> {
-    public String handleRequest(String s, Context context) {
-        return null;
+public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
+
+    @Override
+    public Object handleRequest(Object o, Context context) {
+        if (o instanceof APIGatewayProxyRequestEvent) {
+            System.err.println("API event:\n" + o);
+            return "SUCCESS";
+        } else {
+            throw new RuntimeException("Unsupported event: " + o);
+        }
     }
 }
