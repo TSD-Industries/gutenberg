6a01bd63f2e4984a10428e7f2c0bdc3a0dc42e47
jcavazos <joe@jlcavazos.com>
2023-04-07 10:54:29 -0500

    CF deploy action.

--- src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -1,17 +1,21 @@
 package org.tsd.gutenberg;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
 public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
 
+    private LambdaLogger log;
+
     @Override
     public Object handleRequest(Object o, Context context) {
-        System.err.println("Handling request: " + o);
+        log = context.getLogger();
+        log.log("Handling request:\n" + o);
 
         if (o instanceof APIGatewayProxyRequestEvent) {
-            System.err.println("API event:\n" + o);
+            log.log("API event:\n" + o);
             return "SUCCESS";
         } else {
             throw new RuntimeException("Unsupported event: " + o);
