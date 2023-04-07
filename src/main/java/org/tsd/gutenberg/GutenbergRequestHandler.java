6cef3613fdd2d9948892a945a48c12361aa5311d
jcavazos <joe@jlcavazos.com>
2023-04-07 13:35:48 -0500

    Cron schedule.

--- src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -4,21 +4,31 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class GutenbergRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, Object> {
+public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private LambdaLogger log;
 
     @Override
-    public Object handleRequest(APIGatewayProxyRequestEvent o, Context context) {
+    public Object handleRequest(Object o, Context context) {
         log = context.getLogger();
         log.log("Handling request (" + o.getClass() + "):\n" + o);
 
-        if (o instanceof APIGatewayProxyRequestEvent) {
-            log.log("API event:\n" + o);
-            return "SUCCESS";
-        } else {
-            throw new RuntimeException("Unsupported event (" + o.getClass() + "): " + o);
+        try {
+            final var jsonNode = objectMapper.readTree(o.toString());
+
+            if (o instanceof APIGatewayProxyRequestEvent) {
+                log.log("API event:\n" + o);
+                return "SUCCESS";
+            } else {
+                throw new RuntimeException("Unsupported event (" + o.getClass() + "): " + o);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
