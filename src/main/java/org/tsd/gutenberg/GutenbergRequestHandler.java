d308900a3d7d796d569279d50650619a298288f4
jcavazos <joe@jlcavazos.com>
2023-04-07 22:11:08 -0500

    Cron schedule.

--- src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,12 +16,15 @@ public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
 
     @Override
     public Object handleRequest(Object o, Context context) {
-        log = context.getLogger();
+        setObjectMapper();
+        setLogger(context);
+
         log.log("Handling request (" + o.getClass() + "):\n" + o);
 
         try {
             final var jsonNode = objectMapper.readTree(o.toString());
 
+            log.log("jsonNode:\n" + jsonNode.toString());
             if (o instanceof APIGatewayProxyRequestEvent) {
                 log.log("API event:\n" + o);
                 return "SUCCESS";
@@ -30,5 +34,13 @@ public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void setObjectMapper() {
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    }
+
+    private void setLogger(Context context) {
+        log = context.getLogger();
     }
 }
