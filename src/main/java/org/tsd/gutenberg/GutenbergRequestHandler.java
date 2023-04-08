f35587d4fde2d7402ae5eab6b2edb55d1f8690b5
jcavazos <joe@jlcavazos.com>
2023-04-07 23:35:28 -0500

    Cron schedule.

--- src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
+++ src/main/java/org/tsd/gutenberg/GutenbergRequestHandler.java
@@ -3,12 +3,42 @@ package org.tsd.gutenberg;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+
+import java.util.StringJoiner;
 
 public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
+    static class RestAuthInfo {
+        private final String username;
+        private final String password;
+
+        public RestAuthInfo(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", RestAuthInfo.class.getSimpleName() + "[", "]")
+                    .add("username='" + username + "'")
+                    .add("password='" + password + "'")
+                    .toString();
+        }
+    }
+
+    private static final String SOURCE_FIELD = "source";
+    private static final String SOURCE_EVENTS = "aws.events";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -21,15 +51,32 @@ public class GutenbergRequestHandler implements RequestHandler<Object, Object> {
 
         log.log("Handling request (" + o.getClass() + "):\n" + o);
 
+        final var authInfo = restAuthInfo(context);
+
+        log.log("Auth info: " + authInfo);
+
         final var jsonNode = objectMapper.convertValue(o, JsonNode.class);
 
         log.log("jsonNode:\n" + jsonNode.toString());
-        if (o instanceof APIGatewayProxyRequestEvent) {
-            log.log("API event:\n" + o);
-            return "SUCCESS";
-        } else {
-            throw new RuntimeException("Unsupported event (" + o.getClass() + "): " + o);
+
+        if (isScheduledEvent(jsonNode)) {
+            log.log("Handling scheduled event:\n" + jsonNode);
         }
+
+        return null;
+    }
+
+    private static RestAuthInfo restAuthInfo(Context context) {
+        final var env = context.getClientContext().getEnvironment();
+        return new RestAuthInfo(
+                env.get("WP_REST_USER"),
+                env.get("WP_REST_PASSWORD"));
+    }
+
+    private static boolean isScheduledEvent(JsonNode jsonNode) {
+        return jsonNode.has(SOURCE_FIELD)
+                && jsonNode.get(SOURCE_FIELD).getNodeType().equals(JsonNodeType.STRING)
+                && SOURCE_EVENTS.equalsIgnoreCase(jsonNode.get(SOURCE_FIELD).asText());
     }
 
     private void setObjectMapper() {
